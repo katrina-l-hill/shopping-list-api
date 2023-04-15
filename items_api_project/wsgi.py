65d9013ef94bb4cfb6ffc0529406d4a59d5823a9
Katrina Hill <khill365@gmail.com>
2023-04-15 15:32:12 -0700

    created versel.json file and added setings; updated wsgi.py file with versel settings

--- items_api_project/wsgi.py
+++ items_api_project/wsgi.py
@@ -14,3 +14,6 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'items_api_project.settings')
 
 application = get_wsgi_application()
+
+# https://www.makeuseof.com/django-app-vercel-host-free/?newsletter_popup=1
+app = application
